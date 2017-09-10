22ed74086baa2de68f052ddfaf7202af04aa0652
sgtflow <sgtflow.yt@gmail.com>
2017-09-10 16:52:02 +0200

    Added JavaDoc for the server

--- server/src/main/java/com/florianwoelki/commu/server/CommuServer.java
+++ server/src/main/java/com/florianwoelki/commu/server/CommuServer.java
@@ -17,6 +17,9 @@ import java.util.Date;
 
 /**
  * Created by Florian Woelki on 23.02.17.
+ *
+ * This class creates the GUI for the server.
+ * It also extends from the {@link Window} class.
  */
 public class CommuServer extends Window {
 
@@ -33,6 +36,10 @@ public class CommuServer extends Window {
     private WebTextArea console;
     private WebList listUsers;
 
+    /**
+     * This constructor initializes the frame and
+     * starts the server.
+     */
     public CommuServer() {
         super(TITLE, WIDTH, HEIGHT);
         initFrame();
@@ -41,6 +48,9 @@ public class CommuServer extends Window {
         server = new NetworkServer(8000);
     }
 
+    /**
+     * See {@link Window#createView()}
+     */
     @Override
     public void createView() {
         WebPanel panel = new WebPanel();
@@ -48,6 +58,7 @@ public class CommuServer extends Window {
 
         panel.setLayout(new BorderLayout());
 
+        // Creates the console in the center
         console = new WebTextArea();
         console.setEditable(false);
         ((DefaultCaret) console.getCaret()).setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
@@ -55,6 +66,7 @@ public class CommuServer extends Window {
         consoleSP.setBorder(BorderFactory.createTitledBorder("Console Output:"));
         panel.add(consoleSP, BorderLayout.CENTER);
 
+        // Creates the connected users in the east
         listUsers = new WebList();
         WebScrollPane listUsersSP = new WebScrollPane(listUsers);
         listUsersSP.setBorder(BorderFactory.createTitledBorder("Connected Users:"));
@@ -62,14 +74,21 @@ public class CommuServer extends Window {
         panel.add(listUsersSP, BorderLayout.EAST);
     }
 
+    /**
+     * This method updates the view in the client GUI.
+     * It updates the connected users.
+     */
     public void updateView() {
         DefaultListModel model = new DefaultListModel();
-        server.getConnectedClientMap().keySet().forEach(username -> {
-            model.addElement(username);
-        });
+        server.getConnectedClientMap().keySet().forEach(model::addElement);
         listUsers.setModel(model);
     }
 
+    /**
+     * This method logs a specific message to the console.
+     *
+     * @param message String, which will be logged
+     */
     public void log(String message) {
         console.append(DATE_FORMAT.format(new Date()) + " " + message + "\n");
     }
@@ -79,6 +98,7 @@ public class CommuServer extends Window {
             // Install WebLaf
             WebLookAndFeel.install();
 
+            // Creating instance of server and starts it
             instance = new CommuServer();
             instance.server.startServer();
             instance.setVisible(true);
--- server/src/main/java/com/florianwoelki/commu/server/network/NetworkServer.java
+++ server/src/main/java/com/florianwoelki/commu/server/network/NetworkServer.java
@@ -19,6 +19,9 @@ import java.util.*;
 
 /**
  * Created by Florian Woelki on 24.02.17.
+ *
+ * This class represents the NetworkServer and the logic
+ * for the server sided stuff.
  */
 public class NetworkServer implements PacketListener {
 
@@ -29,17 +32,33 @@ public class NetworkServer implements PacketListener {
     private List<PacketListener> packetListeners = new ArrayList<>();
     private Map<String, Socket> connectedClientMap = new HashMap<>();
 
+    /**
+     * This constructor creates a instance of the NetworkServer.
+     * It also adds the packet listener to the server.
+     *
+     * @param port
+     */
     public NetworkServer(int port) {
         this.port = port;
 
         addPacketListener(this);
     }
 
+    /**
+     * Add a packet listener to the server.
+     *
+     * @param packetListener PacketListener for receiving packets
+     */
     private void addPacketListener(PacketListener packetListener) {
         packetListeners.add(packetListener);
     }
 
+    /**
+     * This method starts the server and starts listening
+     * to incoming packets.
+     */
     public void startServer() {
+        // Starts the server
         try {
             socket = new ServerSocket(port);
             CommuServer.getInstance().log("Server socket initialized on port " + port);
@@ -48,6 +67,15 @@ public class NetworkServer implements PacketListener {
             return;
         }
 
+        startPacketReading();
+
+        running = true;
+    }
+
+    /**
+     * This method starts reading incoming packets.
+     */
+    private void startPacketReading() {
         new Thread(() -> {
             CommuServer.getInstance().log("Listening for clients...");
 
@@ -56,6 +84,7 @@ public class NetworkServer implements PacketListener {
                     Socket client = socket.accept();
                     CommuServer.getInstance().log("Client has connected! " + client.getRemoteSocketAddress());
 
+                    // Starting thread for listening to packets for each client
                     new Thread(() -> {
                         boolean error = false;
                         while(!error && client.isConnected()) {
@@ -79,6 +108,7 @@ public class NetworkServer implements PacketListener {
                             }
                         }
 
+                        // Remove client, if it disconnect or a error occurred
                         removeClient(client);
                     }).start();
                 } catch(IOException e) {
@@ -86,26 +116,45 @@ public class NetworkServer implements PacketListener {
                 }
             }
         }).start();
-
-        running = true;
     }
 
+    /**
+     * This method broadcast a received packet to all packet listener
+     *
+     * @param packet Packet, which will be broadcasted
+     * @param client Socket, who will receive the broadcast
+     */
     private void broadcastPacketReceived(Packet packet, Socket client) {
         packetListeners.forEach(packetListener -> {
             packetListener.packetReceived(packet, client);
         });
     }
 
+    /**
+     * This method broadcast a sent packet to all packet listener
+     *
+     * @param packet Packet, which will be sent
+     * @param client Socket, who will sent the broadcast
+     */
     private void broadcastPacketSent(Packet packet, Socket client) {
         packetListeners.forEach(packetListener -> {
             packetListener.packetSent(packet, client);
         });
     }
 
+    /**
+     * This method stops the server.
+     */
     public void stopServer() {
         running = false;
     }
 
+    /**
+     * See {@link PacketListener#packetSent(Packet, Socket)}
+     *
+     * @param packet Packet, which will be sent
+     * @param client Socket, who receive the packet
+     */
     @Override
     public void packetSent(Packet packet, Socket client) {
         if(packet instanceof ConnectPacket) {
@@ -122,16 +171,29 @@ public class NetworkServer implements PacketListener {
         }
     }
 
+    /**
+     * See {@link PacketListener#packetReceived(Packet, Socket)}
+     *
+     * @param packet Packet, which will be received
+     * @param client Socket, who sent the packet
+     */
     @Override
     public void packetReceived(Packet packet, Socket client) {
-        if(packet instanceof ConnectPacket) {
+        if(packet instanceof ConnectPacket) { // ConnectPacket
             connectClient((ConnectPacket) packet, client);
-        } else if(packet instanceof ChatPacket) {
+        } else if(packet instanceof ChatPacket) { // ChatPacket
             chatClient((ChatPacket) packet, client);
         }
     }
 
+    /**
+     * This method will be called, whenever a client chats.
+     *
+     * @param packet ChatPacket, for knowing information about e.g. the message
+     * @param client Socket, who will sent the ChatPacket
+     */
     private void chatClient(ChatPacket packet, Socket client) {
+        // Send a ChatPacket to all connected clients, that a new client has connected
         connectedClientMap.values().forEach(socket -> {
             if(!socket.getRemoteSocketAddress().equals(client.getRemoteSocketAddress())) {
                 try {
@@ -143,11 +205,19 @@ public class NetworkServer implements PacketListener {
         });
     }
 
+    /**
+     * This method connects a client and notifies all the other
+     * connected clients.
+     *
+     * @param packet ConnectPacket, for knowing information of the connected client
+     * @param client Socket, who will connect to the server
+     */
     private void connectClient(ConnectPacket packet, Socket client) {
         if(connectedClientMap.get(packet.i_username) != null) {
             return;
         }
 
+        // Send the ConnectPacket to all connected clients
         connectedClientMap.keySet().forEach(username -> {
             try {
                 sendPacket(new ConnectPacket(username), client);
@@ -160,6 +230,11 @@ public class NetworkServer implements PacketListener {
         CommuServer.getInstance().updateView();
     }
 
+    /**
+     * This method removes a client from the server and the list.
+     *
+     * @param client Client, who will be removed
+     */
     private void removeClient(Socket client) {
         Iterator<Map.Entry<String, Socket>> iter = connectedClientMap.entrySet().iterator();
         while(iter.hasNext()) {
@@ -168,6 +243,8 @@ public class NetworkServer implements PacketListener {
             Socket socket = entry.getValue();
             if(socket.equals(client)) {
                 iter.remove();
+
+                // Send a DisconnectPacket to all other connected clients
                 connectedClientMap.values().forEach(connectedSocket -> {
                     try {
                         sendPacket(new DisconnectPacket(username), connectedSocket);
@@ -181,11 +258,23 @@ public class NetworkServer implements PacketListener {
         CommuServer.getInstance().updateView();
     }
 
+    /**
+     * This method sends a packet to a specific client.
+     *
+     * @param packet Packet, which will be sent to the client
+     * @param client Socket, who will receive the packet
+     * @throws IOException If something goes wrong
+     */
     private void sendPacket(Packet packet, Socket client) throws IOException {
         DataOutputStream output = new DataOutputStream(client.getOutputStream());
         output.writeUTF(packet.getOutgoingData());
     }
 
+    /**
+     * This method returns the connected clients to the server.
+     *
+     * @return ConnectedClients as a Map
+     */
     public Map<String, Socket> getConnectedClientMap() {
         return connectedClientMap;
     }
