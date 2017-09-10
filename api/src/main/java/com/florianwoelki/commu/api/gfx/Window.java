4f35cb4b515ec804f065efd4a5f5dd409d12883b
sgtflow <sgtflow.yt@gmail.com>
2017-09-10 15:54:13 +0200

    Added JavaDoc for the api

--- api/src/main/java/com/florianwoelki/commu/api/gfx/Window.java
+++ api/src/main/java/com/florianwoelki/commu/api/gfx/Window.java
@@ -8,6 +8,9 @@ import java.awt.*;
 
 /**
  * Created by Florian Woelki on 23.02.17.
+ *
+ * This class represents a Window.
+ * You can create easily a simple frame with custom properties.
  */
 @AllArgsConstructor
 public abstract class Window extends WebFrame {
@@ -16,9 +19,18 @@ public abstract class Window extends WebFrame {
     private int width;
     private int height;
 
+    /**
+     * In this method you need to create the view.
+     * For example if you want to put a button in your window,
+     * you implement it right here.
+     */
     public abstract void createView();
 
-    public void initFrame() {
+    /**
+     * This method initializes the frame with all given properties.
+     * In addition, it calls the method createView(). See also {@link #createView()}.
+     */
+    protected void initFrame() {
         Dimension size = new Dimension(width, height);
         setSize(size);
         setMinimumSize(size);
